f539d16ac2df117e7530b0a4e2007112ac0dac3d
Alex Bolshakov <145608606+Bolshialex@users.noreply.github.com>
2026-01-14 16:15:01 -0800

    Router setup (#8)
    * Clean up
    
    * Add npm packages, creates pages, use react router dom
    
    * Add components and home form
    
    * Add home menu selection component
    
    * Remove assest folder

---
+++ frontEnd/src/components/HomeMenu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function HomeForm() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Skull-King ScoreBoard</h1>
+          <p>Choose and option</p>
+        </div>
+        <div>
+          <button>Start New Game</button>
+          <button>Continue Game</button>
+          <button>Create New Player</button>
+          <button>Lookup Player</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeForm;
---
+++ frontEnd/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeMenu from "../components/HomeMenu";
+
+function Home() {
+  return (
+    <div>
+      <HomeMenu />
+    </div>
+  );
+}
+
+export default Home;
